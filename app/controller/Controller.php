159d3507ae655e15827299aa11ca0277955b3b13
Douglas Sugano <50459465+Drmsugano@users.noreply.github.com>
2024-11-06 21:06:01 -0300

    hf

--- app/controller/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\controller;
-abstract class Controller{
-    public static function isProtected() {
-        if (!isset($_SESSION["usuario_logado"])) {
-            header("Location: /");
-        }
-    }
-}
--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-include "../vendor/autoload.php";
-include "../routes/rotas.php";
